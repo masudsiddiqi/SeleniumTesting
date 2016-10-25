36ba3d0c5f1aa0d3eb8c9acd35c9e1cdb894d6bd
Masud <mdm.siddiqi@gmail.com>
2016-10-24 20:23:45 -0400

    first commit

---
+++ src/filepath/A.java
@@ -0,0 +1,19 @@
+package filepath;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+
+
+public class A {
+	public static void main(String[] args) throws Throwable, IOException {
+		String directory=System.getProperty("user.dir");
+		System.out.println(directory);
+		File file=new File(directory+"/TestData/AppTestData.xlsx");
+	    XSSFWorkbook b= new XSSFWorkbook(file);
+	}
+
+}
